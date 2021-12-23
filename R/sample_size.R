a869415ed7d7cff08ff1f552fb2e5529a945cad2
Jérôme Pasquier <jerome.pasquier@unisante.ch>
2021-12-23 16:39:10 +0100

    update sample size calculation, 2021-11-25, in progress

--- R/sample_size.R
+++ R/sample_size.R
@@ -1,13 +1,32 @@
-# Bibliothèques
+#!/usr/bin/env Rscript
+
+library(geeM)
 library(parallel)
 library(pbapply)
-library(lme4)
-library(lmerTest)
 
-# Répertoire de travail
-if(Sys.info()["nodename"] == "hos45528") {
-  setwd("~/Projects/Consultations/Nanchen David (CATCH)")
+options(mc.cores = detectCores() - 1)
+
+# Arguments
+args = commandArgs(trailingOnly=TRUE)
+
+# Input and output files
+if (FALSE) { # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+if (length(args)==0) {
+  stop("At least one argument must be supplied (input file)")
+} else {
+  input_file <- args[1]
+  if (length(args) > 1) {
+    output_file <- args[2]
+  } else {
+    output_file <- input_file
+  }
 }
+if (!dir.exists(dirname(output_file))) {
+  dir.create(dirname(output_file), recursive = TRUE)
+}
+} # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+input_file <- "ss.csv"
+ss <- read.csv(input_file)
 
 # Initialisation du générateur de nombres aléatoires
 set.seed(333)
@@ -24,24 +43,19 @@ conv_int <- function(p, r) {
   list(sd.logit = d / 2, p.ci = c(expit(logit(p) - d), expit(logit(p) + d)))
 }
 
-# Fichier contenant les résultats
-if(!file.exists("results")) dir.create("results")
-output_file <- "results/sample_size_20191203.txt"
-file.create(output_file)
-
 # Boucle sur les différentes conditions
-for (k in 1:2) {
-
-  if (k == 1) {
-    p <- c(0.5, 0.7)
-    r <- 0.4
-    m <- 4
-  } else {
-    p <- c(0.2, 0.4)
-    r <- c(0.1, 0.3)
-    m <- 4
-  }
-
+ss$sample.size <- NA
+ss$nsim <- NA
+ss$rejection.rate.0 <- NA
+ss$rejection.rate.1 <- NA
+ss$no_conv_prop.0 <- NA
+ss$no_conv_prop.1 <- NA
+for (i in 1:nrow(ss)) {
+  alpha <- ss[i, "alpha"]
+  pwr <- ss[i, "power"]
+  p <- unname(unlist(ss[i, c("pi1", "pi2")]))
+  r <- unname(unlist(ss[i, c("range1", "range2")]))
+  m <- unname(unlist(ss[i, c("size1", "size2")]))
   # Calcul de la taille d'échantillon
   ssize <- function(alpha = .05, power = .9, m, p, r) {
     if (length(m) == 1) m <- c(m, m)
@@ -52,31 +66,13 @@ for (k in 1:2) {
       1 / m[2] * p[2] * (1 - p[2]) + (m[2] - 1) / m[2] * (r[2] / 4)^2
     coef_z * nvar / (p[2] - p[1])^2
   }
-  n <- ssize(m = m, p = p, r = r)
-
+  n <- ceiling(ssize(alpha = alpha, power = pwr, m = m, p = p, r = r))
   # Intervalles des proportions
   p.ci.str <- lapply(1:2, function(j) {
     if (length(r) == 1) r <- c(r, r)
     ci <- conv_int(p[j], r[j])$p.ci
     paste0("[", paste(round(ci, 4), collapse = ","), "]")
   })
-
-  # Output
-  sink(output_file, append = TRUE)
-  cat("------------------- Taille d'échantillon -------------------\n\n")
-  cat(paste("Proportions moyennes dans les groupes contrôles et",
-            "expérimental :", paste(p, collapse = ", "), "\n"))
-  cat(paste("Longueur(s) d'intervalle pour les proportions :",
-            paste(r, collapse = ", "), "\n"))
-  cat(paste("Intervalles pour les proportions :",
-            paste(p.ci.str, collapse = ", "), "\n"))
-  cat(paste("Taille(s) moyenne des familles :",
-            paste(m, collapse = ", "), "\n"))
-  cat(paste("Nombre de famille nécessaire par groupe",
-            "(alpha = .05, power = .9) :", round(n, 2), "\n"))
-  cat("\n\n")
-  sink()
-
   # Simulation de données
   # n: nombre de grappes par groupe (nombre de famille par groupe)
   # m: nombre moyen d'observations par grappe (nombre de proches contactés par 
@@ -93,7 +89,7 @@ for (k in 1:2) {
     if (length(r) == 1) r <- c(r, r)
     s <- sapply(1:2, function(k) conv_int(p[k], r[k])$sd.logit)
     d <- 10^ceiling(log(n + 1, 10))
-    sim_list <- lapply(1:nsim, function(k) {
+    sim_list <- mclapply(1:nsim, function(k) {
       do.call(rbind, lapply(1:2, function(grp) {
         do.call(rbind, lapply(1:n[grp], function(i) {
           J <- round(min(2 * m[grp] + 1, max(1, rnorm(1, m[grp], u[grp]))))
@@ -112,75 +108,42 @@ for (k in 1:2) {
     })
     return(sim_list)
   }
-
   # Simulations sous les hypothèses nulle et alternative
-  if (k == 1) {
-    n2 <- 36
-  } else {
-    n2 <- 28
-  }
-  u <- .5
+  u <- unname(unlist(ss[i, c("sd.size1", "sd.size2")]))
   ns <- 10^3
-  sim0_list <- sim(n = n2, m = m, u = u, p = p[1], r = r, nsim = ns)
-  sim1_list <- sim(n = n2, m = m, u = u, p = p, r = r, nsim = ns)
-
-  # Initialisation du calcul parallèle
-  cl <- makeCluster(detectCores())
-  invisible(clusterEvalQ(cl, library(lme4)))
-
-  # Modèle de régression logistique pour chaque simulation
-  fit <- function(z) {
-    suppressMessages(glmer(
-      y ~ grp + (1 | cluster), family = binomial, data = z,
-      control = glmerControl(optimizer = "bobyqa",
-                             optCtrl = list(maxfun = 10^5))
-    ))
-  }
-  m0_list <- pblapply(sim0_list, fit, cl = cl)
-  m1_list <- pblapply(sim1_list, fit, cl = cl)
-
-  # Proportion de fit singuliers
-  pS0 <- sum(sapply(m0_list, isSingular)) / length(m0_list)
-  pS1 <- sum(sapply(m1_list, isSingular)) / length(m1_list)
-
+  sim0_list <- sim(n = n, m = m, u = u, p = p[1], r = r, nsim = ns)
+  sim1_list <- sim(n = n, m = m, u = u, p = p, r = r, nsim = ns)
   # Taux de rejet sous les hypothèses nulle et alternative
-  rr <- function(m_list, alpha = .05) {
-    v <- pbsapply(m_list, function(m) {
-      coef(summary(m))["grp2", "Pr(>|z|)"] <= alpha
-    })
-    sum(v) / length(v)
+  rr <- function(sim_list) {
+    v <- do.call(base::c, mclapply(sim_list, function(z) {
+      fit <- tryCatch(geem(y ~ grp, id = cluster, family = binomial, data = z),
+                      warning = function(w) w, error = function(e) e)
+      if (any(class(fit) == "geem")) {
+        o <- summary(fit)$p[2] <= alpha
+      } else if (any(class(fit) == "warning")) {
+        if (fit$message == "Did not converge") {
+          o <- NA
+        } else {
+          stop("Other warning in geem")
+        }
+      } else {
+        stop("Other problem in geem")
+      }
+      o
+    }))
+    no_conv_prop <- sum(is.na(v)) / length(v)
+    rate <- sum(v, na.rm = TRUE) / sum(!is.na(v))
+    list(rate = rate, no_conv_prop = no_conv_prop)
   }
-  rr0 <- rr(m0_list)
-  rr1 <- rr(m1_list)
-
-  # Fin du calcul parallèle
-  stopCluster(cl)
-
-  # Output
-  sink(output_file, append = TRUE)
-  cat("----------------------- Simulations ------------------------\n\n")
-  cat(paste("Nombre de simulations:", ns, "(par hypothèse)\n"))
-  cat(paste("Nombre de familles par groupe :", n2, "\n"))
-  cat(paste("Proportions moyennes dans les groupes contrôles et",
-            "expérimental :", paste(p, collapse = ", "), "\n"))
-  cat(paste("Longueur(s) d'intervalle pour les proportions :",
-            paste(r, collapse = ", "), "\n"))
-  cat(paste("Intervalles pour les proportions :",
-            paste(p.ci.str, collapse = ", "), "\n"))
-  cat(paste("Taille(s) moyenne des familles :",
-            paste(m, collapse = ", "), "\n"))
-  cat(paste("Proportion de fit singulier (par hypothèse):", pS0, pS1, "\n"))
-  cat(paste("Taux de rejet (niv=.05) sous l'hypothèse nulle:", rr0, "\n"))
-  cat(paste("Taux de rejet (niv=.05) sous l'alternative:", rr1, "\n"))
-  cat("\n\n")
-  sink()
-
+  rr0 <- rr(sim0_list)
+  rr1 <- rr(sim1_list)
+  # Results
+  ss[i, "sample.size"] <- n
+  ss[i, "nsim"] <- ns
+  ss[i, "rejection.rate.0"] <- rr0$rate
+  ss[i, "rejection.rate.1"] <- rr1$rate
+  ss[i, "no_conv_prop.0"] <- rr0$no_conv_prop
+  ss[i, "no_conv_prop.1"] <- rr1$no_conv_prop
 }
-
-# Session info
-sink(output_file, append = TRUE)
-cat("---------------------- Infos session -----------------------\n\n")
-print(sessionInfo(), locale = FALSE)
-sink()
-
-
+rm(i, alpha, pwr, p, r, m, ssize, n, p.ci.str, sim, u, ns, sim0_list,
+   sim1_list, rr, rr0, rr1)
